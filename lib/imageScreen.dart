6586b2240a5ebf5e22438d826d7dfba1f30b4912
ChoiYoungHan <dnflwlq373@naver.com>
2023-05-02 21:40:47 +0900

    imageScreen 추가

---
+++ lib/imageScreen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class FullscreenImagePage extends StatefulWidget {
+  final List<String> imageUrl;
+  final int Index;
+
+  FullscreenImagePage({required this.imageUrl, this.Index = 1});
+
+  @override
+  _FullscreenImagePageState createState() => _FullscreenImagePageState();
+}
+
+class _FullscreenImagePageState extends State<FullscreenImagePage> {
+  late PageController controller;
+  late int _currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.Index;
+    controller = PageController(initialPage: widget.Index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+              icon: Icon(Icons.arrow_back, color: Colors.grey)
+          ),
+        ),
+        body: PageView.builder(
+          controller: controller,
+          itemCount: widget.imageUrl.length,
+          onPageChanged: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: Image.network('http://182.219.226.49/image/'+ widget.imageUrl[index], fit: BoxFit.contain),
+            );
+          },
+        )
+    );
+  }
+}
